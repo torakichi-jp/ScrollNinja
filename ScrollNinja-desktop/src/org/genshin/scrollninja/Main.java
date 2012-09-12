5f351510c012db2384135f14c0090c2b31b6d457
影月 零 <zero@genshin.org>
2012-09-12 14:06:54 +0900

    initial

---
+++ ScrollNinja-desktop/src/org/genshin/scrollninja/Main.java
@@ -0,0 +1,16 @@
+package org.genshin.scrollninja;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ScrollNinja";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ScrollNinja(), cfg);
+	}
+}
