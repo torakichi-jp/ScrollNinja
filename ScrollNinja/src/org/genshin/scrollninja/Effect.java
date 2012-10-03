2e4fa6ebd67c5fb84454ca50c736075f809341d8
nhs00127 <whirlwind@ubuntu.(none)>
2012-10-03 14:01:45 +0900

    wip

--- ScrollNinja/src/org/genshin/scrollninja/Effect.java
+++ ScrollNinja/src/org/genshin/scrollninja/Effect.java
@@ -1,24 +1,48 @@
+//******************************
+//	Effect.java 
+//******************************
+
 package org.genshin.scrollninja;
 
-public class Effect {
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+public class Effect extends Weapon{
 	
 	//変数宣言
-	private int 		m_Texture;		// テクスチャー
-	private String 	m_Name;		// 名前
-	private int 		m_EffectTime;	// 効果時間
+	private String 	Name;		// 名前
+	private int 		EffectTime;	// 効果時間
+	private Sprite 	Sprite;		// スプライト
+	//private Vector2 	m_Position;	// 座標
+	
 	
 	//コンストラクタ
 	public Effect()
 	{
-		this.m_Name = "";
-		this.m_EffectTime = 5;
+		this.Name       = "";
+		this.EffectTime = 5;
 	}
+		
+	
 
 	//アニメーション
 	public void EffectAnimation()
 	{
+		//-------------------------
+		// 画像切り替え
+		//-------------------------
+		
 	}
 	
-	//
+	public int GetEffectTime()
+	{
+		return this.EffectTime;
+	}
+	
+	// 
+	public void play()
+	{
+		
+	}
 	
 }
--- ScrollNinja/src/org/genshin/scrollninja/Weapon.java
+++ ScrollNinja/src/org/genshin/scrollninja/Weapon.java
@@ -9,30 +9,50 @@ import java.util.ArrayList;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 
 public class Weapon {
 
 	
-	private Texture 	m_Texture;			// テクスチャー
-	private Sprite	m_Sprite;			// スプライト
-	private Vector2 	m_Position;		// 武器座標
-	private float 	m_AttackNum;		// 武器威力
-	private int 		m_WeaponLevel;	// 武器レベル
-	private Boolean 	m_Use;				// 使用フラグ
+	private Sprite	Sprite;		// スプライト
+	private Vector2 	Position;		// 武器座標
+	private float 	AttackNum;		// 武器威力
+	private int 		WeaponLevel;	// 武器レベル
+	private Boolean 	Use;			// 使用フラグ
+	private Body		Body;			// ボディ
 	
 	//コンストラクタ
 	public Weapon()
 	{
-		this.m_Position = new Vector2(0,0);
-		this.m_AttackNum = 1;
-		this.m_WeaponLevel = 1;
+		this.Position    = new Vector2(0,0);
+		this.AttackNum   = 1;
+		this.WeaponLevel = 1;
+		this.Use         = true;
 	}
 	
 	//武器座標ゲット
 	public Vector2 GetWeaponPos()
 	{
-		return this.m_Position;
+		return this.Position;
 	}
 	
+	//武器威力ゲット
+	public float GetAttackNum()
+	{
+		return this.AttackNum;
+	}
 
+	//武器レベルゲット
+	public int GetWeaponLv()
+	{
+		return this.GetWeaponLv();
+	}
+	
+	
 }
+
+
+
+
+
+
