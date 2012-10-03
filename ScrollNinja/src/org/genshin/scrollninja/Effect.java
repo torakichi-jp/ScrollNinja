8d0824b5eab7bd503bf45a22e934939f1bb522f6
R-Y <k_nazuki@hkg.odn.ne.jp>
2012-10-03 13:55:57 +0900

    anime

--- ScrollNinja/src/org/genshin/scrollninja/Effect.java
+++ ScrollNinja/src/org/genshin/scrollninja/Effect.java
@@ -1,12 +1,12 @@
 package org.genshin.scrollninja;
 
 public class Effect {
-	
+
 	//変数宣言
-	private int 		m_Texture;		// テクスチャー
-	private String 	m_Name;		// 名前
-	private int 		m_EffectTime;	// 効果時間
-	
+	private int 	m_Texture;		// テクスチャー
+	private String 	m_Name;			// 名前
+	private int 	m_EffectTime;	// 効果時間
+
 	//コンストラクタ
 	public Effect()
 	{
@@ -18,7 +18,7 @@ public class Effect {
 	public void EffectAnimation()
 	{
 	}
-	
+
 	//
-	
+
 }
--- ScrollNinja/src/org/genshin/scrollninja/Weapon.java
+++ ScrollNinja/src/org/genshin/scrollninja/Weapon.java
@@ -1,5 +1,5 @@
 //******************************
-//	Weapon.java 
+//	Weapon.java
 //******************************
 
 package org.genshin.scrollninja;
@@ -12,14 +12,13 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Weapon {
 
-	
-	private Texture 	m_Texture;			// テクスチャー
-	private Sprite	m_Sprite;			// スプライト
+	private Texture 	m_Texture;		// テクスチャー
+	private Sprite		m_Sprite;		// スプライト
 	private Vector2 	m_Position;		// 武器座標
-	private float 	m_AttackNum;		// 武器威力
+	private float 		m_AttackNum;	// 武器威力
 	private int 		m_WeaponLevel;	// 武器レベル
-	private Boolean 	m_Use;				// 使用フラグ
-	
+	private Boolean 	m_Use;			// 使用フラグ
+
 	//コンストラクタ
 	public Weapon()
 	{
@@ -27,12 +26,12 @@ public class Weapon {
 		this.m_AttackNum = 1;
 		this.m_WeaponLevel = 1;
 	}
-	
+
 	//武器座標ゲット
 	public Vector2 GetWeaponPos()
 	{
 		return this.m_Position;
 	}
-	
+
 
 }
