116f0e437e470c86212b18fefd2f0224c7d92b6f
R-Y <k_nazuki@hkg.odn.ne.jp>
2012-10-03 15:09:10 +0900

    3日15時マージ

--- ScrollNinja/src/org/genshin/scrollninja/Weapon.java
+++ ScrollNinja/src/org/genshin/scrollninja/Weapon.java
@@ -13,44 +13,28 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 public class Weapon {
 
-<<<<<<< HEAD
-	private int			m_WeaponNum;		// 武器番号
-	private Sprite		m_Sprite;			// スプライト
-	private Vector2 	m_Position;			// 武器座標
-	private float 		m_AttackNum;		// 武器威力
-	private int 		m_WeaponLevel;		// 武器レベル
-	private Boolean 	m_Use;				// 使用フラグ
-	
-	//コンストラクタ
-	public Weapon(int i) {
-		this.m_Position = new Vector2(0,0);
-		this.m_AttackNum = 1;
-		this.m_WeaponLevel = 1;
-		m_WeaponNum = i;
-=======
-	
 	private Sprite	sprite;		// スプライト
 	private Vector2 	position;		// 武器座標
 	private float 	attackNum;		// 武器威力
 	private int 		weaponLevel;	// 武器レベル
 	private Boolean 	use;			// 使用フラグ
 	private Body		body;			// ボディ
-	
+
 
 	//コンストラクタ
-	public Weapon() {
+	public Weapon(int i) {
 		this.position    = new Vector2(0,0);
 		this.attackNum   = 0;
 		this.weaponLevel = 0;
 		this.use         = true;
->>>>>>> kimura/master
+
 	}
 
 	//武器座標ゲット
 	public Vector2 GetWeaponPos() {
 		return this.position;
 	}
-	
+
 	//武器威力ゲット
 	public float GetAttackNum() {
 		return this.attackNum;
@@ -61,8 +45,8 @@ public class Weapon {
 	public int GetWeaponLv() {
 		return this.GetWeaponLv();
 	}
-	
-	
+
+
 }
 
 
