50ff8dcd7fac7380c6639978250611752bc8fbf0
Umemura <superkabochance1123@gmail.com>
2012-10-05 12:04:51 +0900

    プレイヤー更新、メインメニューの作成

---
+++ ScrollNinja/src/org/genshin/scrollninja/GameMain.java
@@ -0,0 +1,100 @@
+package org.genshin.scrollninja;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameMain implements Screen{
+	private Game					ScrollNinjya;
+	private OrthographicCamera		camera;				// カメラ
+	private SpriteBatch				spriteBatch;		// スプライトバッチ
+	
+	public GameMain() {
+		camera				= new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		spriteBatch 		= new SpriteBatch();
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	@Override
+	public void render(float delta) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
