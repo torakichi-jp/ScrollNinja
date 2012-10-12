726c758031e68bcfe56cbd3def32582e3872cc5b
R-Y <k_nazuki@hkg.odn.ne.jp>
2012-10-12 17:28:35 +0900

    ScrollNinja.javaを少しさわった

--- ScrollNinja/src/org/genshin/scrollninja/ScrollNinja.java
+++ ScrollNinja/src/org/genshin/scrollninja/ScrollNinja.java
@@ -3,26 +3,31 @@ package org.genshin.scrollninja;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class ScrollNinja extends Game {
 
 	public static Texture texture;
 	public static Sprite stageSpr;
 	public static Sprite bgSpr;
-	
-	
+	public static Vector2 window;
+
+
 	@Override
 	public void create() {
 		/**
 			事前に初期設定が必要ならここで
 		*/
+
+		window = new Vector2();
+		window.x = Gdx.graphics.getWidth();
+		window.y = Gdx.graphics.getHeight();
+
 		Background.LoadTexture();		// 背景読み込み
 		setScreen(new MainMenu(this));	// メインメニュー読み込み
 	}
-	
+
 
 	@Override
 	public void dispose() {
