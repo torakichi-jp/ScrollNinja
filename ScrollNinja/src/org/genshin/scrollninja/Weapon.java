08e851c128194dd38c932d0bee459860a7f52a09
kimura <ridlanc@gmail.com>
2012-10-16 14:52:14 +0900

    weapon

--- ScrollNinja/src/org/genshin/scrollninja/Weapon.java
+++ ScrollNinja/src/org/genshin/scrollninja/Weapon.java
@@ -27,31 +27,29 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 public class Weapon extends ObJectBase{
 
 	private String	name;			// 名前
-	private Sprite	sprite;			// スプライト
-	private Vector2 position;		// 武器座標
+	private Sprite	sprite;		// スプライト
+	private Vector2 	position;		// 武器座標
 	private float 	attackNum;		// 武器威力
-	private int 	weaponLevel;	// 武器レベル
-	private Boolean use;			// 使用フラグ
-	private Body	body;			// ボディ
+	private int 		weaponLevel;	// 武器レベル
+	private Boolean 	use;			// 使用フラグ
+	private Body		body;			// ボディ
 	private boolean	ShootFlag;		// シュートフラグ(手裏剣)
-
+	private int 		deleteTime;	// 手裏剣消去時間
+	private Vector2 	velocity;		// 移動用速度
+	private boolean 	FlyingFlag;
+	
 	private Player player;
 	private Enemy enemy;
 
 	private static final float FIRST_SPEED	=  30f;		// 初速度
 	private static final float GRAVITY		= -20f;		// 重力
 	private static final int   MAX_WIDTH	= 100;
-	private Vector2 velocity;							// 移動用速度
-	private boolean FlyingFlag;
-
-
-	//private static final int AADD = 10;
 
 	//コンストラクタ
 	public Weapon(String Name) {
 		//enemy = EnemyManager.GetEnemy("1");
 		name			 = new String(Name);
-		this.position    = new Vector2(0,-20);
+		this.position    = new Vector2(0,-20);		// 初期位置は画面外
 		this.attackNum   = 0;
 		this.weaponLevel = 0;
 		this.use         = true;
@@ -97,7 +95,8 @@ public class Weapon extends ObJectBase{
 		body.setBullet(true);			// すり抜け防止
 		body.setFixedRotation(true);	// シミュレーションでの自動回転をしない
 		body.setTransform(position, 0);		// 初期位置
-
+		
+		deleteTime = 120;		// 120fで消える
 
 	}
 
@@ -109,43 +108,62 @@ public class Weapon extends ObJectBase{
 		position = body.getPosition();
 		body.setTransform(position ,0);
 
-
-
-		sprite.setPosition(position.x + /*velocity.x*/ - 32, position.y - 32);
-		sprite.setRotation((float) (body.getAngle()*180/Math.PI));
-
 		//if(ShootFlag)
 		//body.setTransform(position, body.getAngle());
 		//body.setLinearVelocity(position.x , 0);
 
 		shuriken();
-
+		
 		System.out.println(player.position.y);
 
 	}
 
 	// 手裏剣の動き
 	public void shuriken() {
+		
+		sprite.setPosition(position.x - 32, position.y - 32);
+		sprite.setRotation((float) (body.getAngle()*180/Math.PI));
+		
+		// 手裏剣表示時間
+		deleteTime += 1;
+		
 		if (Gdx.input.isKeyPressed(Keys.S)) {
 			FlyingFlag = true;
 			ShootFlag = true;
+			// 押されたら手裏剣の座標を敵の位置へ移動
+			position = enemy.position;
 		}
 
+		// debug
 		if(FlyingFlag) {
 			//position.x += 1.0f;
 			// 向きで飛ぶ方向決定
 			if(enemy.GetDirection() == 1) {
+				// 加速度加算
 				velocity.x += 1.0f;
 			}
 			else {
+				// 加速度減算
 				velocity.x -= 1.0f;
 			}
-
+			
+			// 敵とプレイヤーの高低チェック
+			if(enemy.position.y > player.position.y) {
+				// 敵の方が上にいる場合
+				velocity.y += 0.1f;
+			}
+			else if(enemy.position.y < player.position.y) {
+				// プレイヤーのほうが上にいる場合
+				velocity.y -= 0.1f;	
+			}
 		}
-
-		//if( > MAX_WIDTH) {
-			//sprite.setPosition(50,-10);
-
+		
+		if(deleteTime >= 120 && FlyingFlag) {
+			// 120fたったら手裏剣を画面外へ
+			sprite.setPosition(0, -60);
+		}
+		
+		
 	}
 
 	// 描画
