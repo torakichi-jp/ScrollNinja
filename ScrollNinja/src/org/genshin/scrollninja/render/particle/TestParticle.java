25778b33248b4892d4955eba9c39d11f7a93a98e
torakichi-jp <airothick@gmail.com>
2013-07-03 12:26:22 +0900

    パーティクルの表示ができた（サイズもOK）

--- ScrollNinja/src/org/genshin/scrollninja/render/particle/TestParticle.java
+++ ScrollNinja/src/org/genshin/scrollninja/render/particle/TestParticle.java
@@ -3,8 +3,11 @@ package org.genshin.scrollninja.render.particle;
 import org.genshin.scrollninja.Global;
 import org.genshin.scrollninja.GlobalDefine;
 import org.genshin.scrollninja.render.AbstractRenderObject;
+import org.genshin.scrollninja.utils.debug.DebugTool;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.ParticleEmitter.ScaledNumericValue;
 import com.badlogic.gdx.math.Vector2;
 
@@ -29,14 +32,34 @@ public class TestParticle extends AbstractRenderObject {
 				Gdx.files.internal("data/particles"));
 		
 		// サイズ変更
-		ScaledNumericValue value = particleEffect.getEmitters().get(0).getScale();
-		float[] scaling = new float[1];
-		scaling[0] = GlobalDefine.INSTANCE.WORLD_SCALE;
-		value.setScaling(scaling);
-		//value = particleEffect.getEmitters().get(0).getSpawnHeight();
+		//ParticleEmitter emitter = particleEffect.getEmitters().get(0);
+		//ScaledNumericValue value = emitter.getScale();
+		//float[] scaling = new float[1];
+		//scaling[0] = GlobalDefine.INSTANCE.WORLD_SCALE;
 		//value.setScaling(scaling);
-		//value = particleEffect.getEmitters().get(0).getSpawnWidth();
-		//value.setScaling(scaling);
+		//value.setHighMax(value.getHighMax() * scaling[0]);
+		//value.setHighMin(value.getHighMin() * scaling[0]);
+
+		float scale = GlobalDefine.INSTANCE.WORLD_SCALE;
+		ParticleEmitter emitter = particleEffect.getEmitters().get(0);
+
+		DebugTool.logToConsole(""+emitter.getVelocity().getHighMax());
+		DebugTool.logToConsole(""+emitter.getVelocity().getHighMin());
+		DebugTool.logToConsole(""+emitter.getVelocity().getLowMax());
+		DebugTool.logToConsole(""+emitter.getVelocity().getLowMin());
+		
+		float scalingMax = emitter.getScale().getHighMax();
+		float scalingMin = emitter.getScale().getHighMin();
+		emitter.getScale().setHigh(scalingMin * scale, scalingMax * scale);
+		scalingMax = emitter.getScale().getLowMax();
+		scalingMin = emitter.getScale().getLowMin();
+		emitter.getScale().setLow(scalingMin * scale, scalingMax * scale);
+		scalingMax = emitter.getVelocity().getHighMax();
+		scalingMin = emitter.getVelocity().getHighMin();
+		emitter.getVelocity().setHigh(scalingMin * scale, scalingMax * scale);
+		scalingMax = emitter.getVelocity().getLowMax();
+		scalingMin = emitter.getVelocity().getLowMin();
+		emitter.getVelocity().setLow(scalingMin * scale, scalingMax * scale);
 		
 		// 位置設定
 		this.position = new Vector2(0, -5.0f);
